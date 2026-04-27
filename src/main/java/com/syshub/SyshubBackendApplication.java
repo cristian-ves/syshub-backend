6ad8e5feceffb0da46a2c5e55d92d7981666bc16
aleja <alejandrovasquezesc@gmail.com>
2026-04-26 21:11:00 -0600

    chore: initialize project structure with layered architecture

---
+++ src/main/java/com/syshub/SyshubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.syshub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyshubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyshubBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/syshub/SyshubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syshub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyshubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
