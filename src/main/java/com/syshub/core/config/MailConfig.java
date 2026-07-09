51f7393635b3e2c377021325fc828b182bbff1ba
aleja <alejandrovasquezesc@gmail.com>
2026-07-09 11:58:48 -0600

    fix(mail): update mail configuration to use SSL on port 465

--- src/main/java/com/syshub/core/config/MailConfig.java
+++ src/main/java/com/syshub/core/config/MailConfig.java
@@ -34,7 +34,8 @@ public class MailConfig {
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.ssl.enable", "true");
+        props.put("mail.smtp.starttls.enable", "false");
         props.put("mail.debug", "true");
 
         return mailSender;
