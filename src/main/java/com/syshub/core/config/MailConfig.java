3f47284f8a9a08187ac6cbf03f0b42c669d92b76
aleja <alejandrovasquezesc@gmail.com>
2026-08-06 13:19:09 -0600

    fix(mail): update MailConfig Java bean to use STARTTLS and disable implicit SSL

--- src/main/java/com/syshub/core/config/MailConfig.java
+++ src/main/java/com/syshub/core/config/MailConfig.java
@@ -34,8 +34,8 @@ public class MailConfig {
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.ssl.enable", "true");
-        props.put("mail.smtp.starttls.enable", "false");
+        props.put("mail.smtp.ssl.enable", "false");
+        props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.debug", "true");
 
         return mailSender;
